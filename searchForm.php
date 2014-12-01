90ceed823af0413e515e88eb2741054ff70c5a83
nishmar <nishmar.c@gmail.com>
2014-12-01 11:08:40 -0500

    Refactored from html file to include sessions

--- searchForm.php
+++ searchForm.php
@@ -1,3 +1,9 @@
+<?php
+// Start the session
+session_start();
+$_SESSION["userID"] = "test2";
+?>
+
 <!DOCTYPE html>
 <html>
 <head lang="en">
@@ -5,10 +11,16 @@
     <title>Game Search</title>
 </head>
 
-<h1>Game Search</h1><br>
+<h1>Search</h1><br>
 <form action="search.php" method="get" >
     <input type="hidden" name="pagenum" value="1">
     Search for a game: <input type="text" name="search_term"><br>
+    <h2> Search in </h2>
+    <select name='type'>
+        <option value='game'> games </option>
+        <option value='user'> users </option>
+    </select>
+
     <input type="submit">
 </form>
 
