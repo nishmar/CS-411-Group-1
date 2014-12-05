612940e366eb09ff8bc545818f38154b4a7827a3
nishmar <nishmar.c@gmail.com>
2014-12-05 16:34:27 -0500

    Fixed blank login bug

--- login.php
+++ login.php
@@ -8,29 +8,31 @@ mysql_select_db('login', $conn);
 $username = mysql_real_escape_string($username);
 $query = "SELECT *
         FROM `gamecache`.`userlist`
-        WHERE `User ID` = '$username';";
+        WHERE `User ID` = '$username'";
 
 $result = mysql_query($query);
 if(mysql_num_rows($result) == 0) // User not found. So, redirect to login_form again.
 {
     header('Location: login.html');
 }
+else {
 
-$userData = mysql_fetch_array($result, MYSQL_ASSOC);
+    $userData = mysql_fetch_array($result, MYSQL_ASSOC);
 
-echo '<script language="javascript">';
-echo 'alert("message successfully sent")';
-echo '</script>';
+    echo '<script language="javascript">';
+    echo 'alert("message successfully sent")';
+    echo '</script>';
 
-if($password != $userData['Password']) // Incorrect password. So, redirect to login_form again.
-{
-    header('Location: login.html');
-}else{ // Redirect to home page after successful login.
-    session_regenerate_id();
-    //$_SESSION['sess_user_id'] = $userData['id'];
+    if ($password != $userData['Password']) // Incorrect password. So, redirect to login_form again.
+    {
+        header('Location: login.html');
+    } else { // Redirect to home page after successful login.
+        session_regenerate_id();
+        //$_SESSION['sess_user_id'] = $userData['id'];
 
-    $_SESSION['userID'] = $userData['username'];
-    session_write_close();
-    header('Location: searchForm.php');
+        $_SESSION['userID'] = $userData['username'];
+        session_write_close();
+        header('Location: searchForm.php');
+    }
 }
 ?>
